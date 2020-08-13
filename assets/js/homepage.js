afd265cb8925d9b03565f3ffc7f328b0a675e2d3
D-Know <n.dino90@gmail.com>
2020-08-13 13:23:31 -0700

    finishing touches clear button

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,69 +1,66 @@
 var currentWeatherEl = document.querySelector("#current-Weather");
 var searchHistoryEl = document.querySelector("#search-history");
 var fiveDayEl = document.querySelector("#five-day")
-var historyCounter = localStorage.length
+var clearButton = document.querySelector("#clear-button")
+//array to be used in local storage
 var prevCities = []
+//pulling the array back out from its string form in local storage
 var cityHistory = JSON.parse(window.localStorage.getItem('searchedCities'))
 
-function displayHistory(){
+//publishes the search history to the page
+function displayHistory() {
+    //get rid of duplicate searches
     var uniqueCities = [...new Set(cityHistory)]
-    
+
     var listGroup = document.querySelector("#cityList")
 
-    
-
-    
-    
-    if(!cityHistory){
+    if (!cityHistory) {
         console.log('local storage empty')
-        searchHistoryEl.innerHTML= ""
-    }
-    else{
-        for(i=0; i < uniqueCities.length; i++){
-        var searchList = document.createElement('a');
-        searchList.href = "#"
-        searchList.classList.add('list-group-item', 'list-group-item-action');
+        searchHistoryEl.innerHTML = ""
+    } else {
+        for (i = 0; i < uniqueCities.length; i++) {
+            var searchList = document.createElement('a');
+            searchList.href = "#"
+            searchList.classList.add('list-group-item', 'list-group-item-action');
 
-        searchList.innerHTML =`
+            searchList.innerHTML = `
         ${uniqueCities[i]}
         `
-
-        listGroup.appendChild(searchList)
+            listGroup.appendChild(searchList)
         }
-                
-    
     }
 }
 
-function searchHistory(city){
+//clears history
+function clearHistory(){
+    searchHistoryEl.innerHTML = ""
+    window.localStorage.clear()
+}
 
-    if(cityHistory){
+function searchHistory(city) {
+
+    if (cityHistory) {
         let prevCities = cityHistory
         console.log("search Histroy", city)
         prevCities.push(city)
         console.log("prev cities array", prevCities)
         localStorage.setItem('searchedCities', JSON.stringify(prevCities))
-    }else{
+    } else {
         prevCities.push(city)
         console.log("prev cities array", prevCities)
         localStorage.setItem('searchedCities', JSON.stringify(prevCities))
     }
-    
-
 }
 
 
 function displayCurrentWeather(info, cityName) {
-    
     var temp = info.current.temp;
-
     //originally calculated temperature Didn't know I could get it in imperial
     // let tempInF = (kelvinTemp - 273.15) * (9 / 5) + 32;
     // let currentTemp = Math.round(tempInF)
     // console.log(temp)
 
-    var windSpeed =info.current.wind_speed
-
+    var windSpeed = info.current.wind_speed
     //originally calculated imperial conversions.  
     // var meterWindSpeed = info.current.wind_speed
     // let windMph = meterWindSpeed * 2.237
@@ -76,7 +73,15 @@ function displayCurrentWeather(info, cityName) {
     var uvIndex = info.current.uvi
     // console.log(uvIndex)
 
-    
+    //make the class for the UV Index change color
+    var uvClass = ''
+    if (uvIndex < 4) {
+        uvClass = '"bg-success"'
+    } else if (uvIndex > 4 && uvIndex < 8) {
+        uvClass = '"bg-warning"'
+    } else if (uvIndex > 8) {
+        uvClass = '"bg-danger"'
+    }
 
     // console.log('from display func', cityName)
 
@@ -90,7 +95,7 @@ function displayCurrentWeather(info, cityName) {
     <p class="card-text">Temperature: ${temp}</p>
     <p class="card-text">Humidity: ${currentHumidity}% </p>
     <p class="card-text">Wind Speed: ${windSpeed} MPH </p>
-    <p class="card-text">UV Index: ${uvIndex} </p>
+    <p class="card-text">UV Index: <span class=${uvClass}>  ${uvIndex}  </span> </p>
     `
     var currentContainer = document.querySelector("#current-weather")
 
@@ -102,7 +107,7 @@ function displayForecast(info) {
 
     // var firstHumidity = daily[0].humidity
     // console.log(firstHumidity)
-   
+
 
     for (i = 1; i < 6; i++) {
         var humidity = daily[i].humidity;
@@ -110,30 +115,30 @@ function displayForecast(info) {
         var temp = daily[i].temp.day
         var dailyWeather = daily[i].weather
         // console.log(dailyWeather)
-        let ikon= dailyWeather[0].icon
-        var dateUnix= daily[i].dt
+        let ikon = dailyWeather[0].icon
+        var dateUnix = daily[i].dt
 
         //formula to get date out of unix time
         const timeMillis = dateUnix * 1000;
 
         const dateObject = new Date(timeMillis)
 
-        const humanDateFormat = dateObject.toLocaleString() 
+        const humanDateFormat = dateObject.toLocaleString()
 
         // console.log(humanDateFormat)
 
-        let splitDate= humanDateFormat.split(',')
+        let splitDate = humanDateFormat.split(',')
         // console.log(splitDate)
         let finalDate = splitDate[0]
         // console.log(finalDate)
 
         console.log('humidity=', humidity, 'temp =', temp, 'icon', ikon, 'date', finalDate)
 
-            var forcastDiv = document.createElement('div');
-            forcastDiv.classList.add('col', 'card', 'bg-light');
+        var forcastDiv = document.createElement('div');
+        forcastDiv.classList.add('col', 'card', 'bg-light');
 
-            // add the inner html
-            forcastDiv.innerHTML = `
+        // add the inner html
+        forcastDiv.innerHTML = `
             <div class="card-body text-center">
                             <p class="card-text font-weight-bold">${finalDate} </p>
                             <img src="http://openweathermap.org/img/wn/${ikon}@2x.png" alt="alternatetext">
@@ -141,9 +146,9 @@ function displayForecast(info) {
                             <p class="card-text ">Humidity: ${humidity}% </p>
                         </div>
             `
-            var forcastContainer = document.querySelector("#forcast-cards")
+        var forcastContainer = document.querySelector("#forcast-cards")
 
-            forcastContainer.appendChild(forcastDiv)
+        forcastContainer.appendChild(forcastDiv)
 
 
 
@@ -178,7 +183,7 @@ function getCurrentWeather(city) {
                 response.json().then(function (data) {
                     // console.log(data)
                     //displayRepos(data, city)
-                    
+
                     // localStorage.setItem(historyCounter, JSON.stringify(city))
                     // historyCounter++
                     searchHistory(city)
@@ -198,16 +203,17 @@ function getCurrentWeather(city) {
 }
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city")
+var searchBtn = document.querySelector("#search")
 
 
 // takes in city input 
 function formSubmitHandler(event) {
     event.preventDefault();
     const currentContainer = document.querySelector('#current-weather')
-    currentContainer.innerHTML= ''
+    currentContainer.innerHTML = ''
 
     const forcastContainer = document.querySelector("#forcast-cards")
-    forcastContainer.innerHTML= ''
+    forcastContainer.innerHTML = ''
 
     //gets rid of spaces
     var city = cityInputEl.value.trim();
@@ -229,6 +235,9 @@ function formSubmitHandler(event) {
 
 displayHistory()
 
+
+
 //listen for submit button
 cityFormEl.addEventListener("submit", formSubmitHandler);
 
+clearButton.addEventListener("reset", clearHistory)
